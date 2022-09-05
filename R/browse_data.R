5b3421fefe40cf07720b3112eff1ed60731db5e1
Michal Burda <michal.burda@osu.cz>
2022-09-05 14:34:36 +0200

    Add filters + summary to browse_data()

--- R/browse_data.R
+++ R/browse_data.R
@@ -2,60 +2,145 @@
 #' @return
 #' @author Michal Burda
 #' @export
-#' @importFrom shiny fillCol
-#' @importFrom shiny fillRow
 #' @importFrom shiny div
 #' @importFrom shiny includeHTML
 #' @importFrom shiny uiOutput
 #' @importFrom shiny renderUI
+#' @importFrom shiny plotOutput
+#' @importFrom shiny renderPlot
 #' @importFrom shiny req
-#' @importFrom shiny dialogViewer
-#' @importFrom shiny runGadget
 #' @importFrom shiny shinyApp
-#' @importFrom miniUI miniPage
-#' @importFrom miniUI miniContentPanel
-#' @importFrom miniUI gadgetTitleBar
+#' @importFrom shiny fluidPage
+#' @importFrom shiny titlePanel
+#' @importFrom shiny sidebarLayout
+#' @importFrom shiny sidebarPanel
+#' @importFrom shiny mainPanel
+#' @importFrom shiny tabsetPanel
+#' @importFrom shiny tabPanel
+#' @importFrom shiny reactive
+#' @importFrom shiny checkboxInput
+#' @importFrom shiny sliderInput
+#' @importFrom shiny selectInput
 #' @importFrom DT datatable
 #' @importFrom DT renderDataTable
 #' @importFrom DT dataTableOutput
 #' @importFrom tools Rd2HTML
 #' @importFrom tools Rd_db
-browse_data <- function(datasets = list_data(TRUE)) {
+#' @importFrom ggplot2 ggplot
+#' @importFrom ggplot2 geom_histogram
+#' @importFrom ggplot2 aes
+browse_data <- function(data_list = list_data(TRUE)) {
     window_name <- 'Package Data Browser'
+    anyChoice <- '-- any --'
+    packageChoices <- c(anyChoice, levels(data_list$package))
+    classChoices <- c(anyChoice, levels(data_list$class))
+    modeChoices <- c(anyChoice, levels(data_list$mode))
+    typeChoices <- c(anyChoice, levels(data_list$type))
 
-    ui <- miniPage(
-        gadgetTitleBar(window_name),
-        fillCol(
-            miniContentPanel(
-                div(dataTableOutput('dataset_table'), style = 'font-size: 100%')
+    createSlider <- function(columnName, label) {
+        sliderInput(columnName,
+                    label,
+                    step = 1,
+                    min = min(data_list[[columnName]], na.rm = TRUE),
+                    max = max(data_list[[columnName]], na.rm = TRUE),
+                    value = c(min(data_list[[columnName]], na.rm = TRUE),
+                              max(data_list[[columnName]], na.rm = TRUE)))
+    }
+
+    filterEquality <- function(d, column, value, naAllowed) {
+        naCondition <- FALSE
+        if (naAllowed) {
+            naCondition <- is.na(d[[column]])
+        }
+        choiceCondition <- TRUE
+        if (value != anyChoice) {
+            choiceCondition <- !is.na(d[[column]]) & (d[[column]] == value)
+        }
+
+        d[naCondition | choiceCondition, ]
+    }
+
+    filterRange <- function(d, column, values, naAllowed) {
+        naCondition <- FALSE
+        if (naAllowed) {
+            naCondition <- is.na(d[[column]])
+        }
+
+        d[naCondition | (!is.na(d[[column]]) & d[[column]] >= values[1] & d[[column]] <= values[2]), ]
+    }
+
+    ui <- fluidPage(
+        titlePanel(window_name),
+        sidebarLayout(
+            sidebarPanel(
+                selectInput('package', 'package', packageChoices),
+                selectInput('class', 'class', classChoices),
+                selectInput('mode', 'mode', modeChoices),
+                selectInput('type', 'type', typeChoices),
+                createSlider('records', 'records'),
+                createSlider('variables', 'variables'),
+                createSlider('numericVariables', 'numeric variables'),
+                createSlider('factorVariables', 'factor variables'),
+                createSlider('characterVariables', 'character variables'),
+                createSlider('logicalVariables', 'logical variables'),
+                checkboxInput('loadable', 'loadable', value = TRUE),
+                checkboxInput('naAllowed', 'allow NA values', value = TRUE)
             ),
-            miniContentPanel(
-                div(DT::dataTableOutput('data_table'), style = 'font-size: 100%')
-            ),
-            #miniContentPanel(
-                #uiOutput('help', inline = TRUE)
-            #)
+            mainPanel(
+                dataTableOutput('dataset_table'),
+                tabsetPanel(type = 'tabs',
+                    tabPanel('summary',
+                        fluidRow(
+                            column(4, plotOutput('records_plot', height = '200px')),
+                            column(4, plotOutput('variables_plot', height = '200px'))
+                        ),
+                        fluidRow(
+                            column(4, plotOutput('class_plot', height = '200px')),
+                            column(4, plotOutput('mode_plot', height = '200px')),
+                            column(4, plotOutput('type_plot', height = '200px'))
+                        )
+                    ),
+                    tabPanel('dataset', dataTableOutput('data_table')),
+                    tabPanel('description', uiOutput('help'))
+                )
+            )
         )
     )
 
     server <- function(input, output, session) {
         tmp <- tempfile()
 
+        datasets <- reactive({
+            result <- data_list
+            result <- filterEquality(result, 'loadable', input$loadable, input$naAllowed)
+            result <- filterEquality(result, 'package', input$package, input$naAllowed)
+            result <- filterEquality(result, 'class', input$class, input$naAllowed)
+            result <- filterEquality(result, 'mode', input$mode, input$naAllowed)
+            result <- filterEquality(result, 'type', input$type, input$naAllowed)
+            result <- filterRange(result, 'records', input$records, input$naAllowed)
+            result <- filterRange(result, 'variables', input$variables, input$naAllowed)
+            result <- filterRange(result, 'numericVariables', input$numericVariables, input$naAllowed)
+            result <- filterRange(result, 'factorVariables', input$factorVariables, input$naAllowed)
+            result <- filterRange(result, 'characterVariables', input$characterVariables, input$naAllowed)
+            result <- filterRange(result, 'logicalVariables', input$logicalVariables, input$naAllowed)
+
+            result
+        })
+
         output$dataset_table <- DT::renderDataTable({
-            datatable(datasets,
+            datatable(datasets(),
                       rownames = FALSE,
                       filter = 'none',
                       selection = 'single',
                       class = 'compact nowrap hover order-column stripe',
-                      options = list(dom = 't', # remove search input
-                                     paging = FALSE))
+                      options = list(paging = TRUE))
         })
 
         output$help <- renderUI({
             selected <- req(input$dataset_table_rows_selected)
             if (!is.null(selected)) {
-                name <- datasets[selected, 'name']
-                package <- datasets[selected, 'package']
+                name <- datasets()[selected, 'name']
+                package <- as.character(datasets()[selected, 'package'])
                 rdfile <- paste0(name, '.Rd')
                 Rd2HTML(Rd_db(package)[[rdfile]], tmp, no_links = TRUE, package=package)
                 includeHTML(tmp)
@@ -66,8 +151,8 @@ browse_data <- function(datasets = list_data(TRUE)) {
             selected <- req(input$dataset_table_rows_selected)
             d <- data.frame()
             if (!is.null(selected)) {
-                name <- datasets[selected, 'name']
-                package <- datasets[selected, 'package']
+                name <- datasets()[selected, 'name']
+                package <- as.character(datasets()[selected, 'package'])
                 d <- load_data(name, package)
             }
             datatable(d,
@@ -77,9 +162,27 @@ browse_data <- function(datasets = list_data(TRUE)) {
                       class = 'compact nowrap hover order-column stripe',
                       options = list(paging = TRUE))
         })
+
+        output$records_plot <- renderPlot({
+            ggplot(datasets()) + geom_histogram(aes(x = records), bins = 30)
+        })
+
+        output$variables_plot <- renderPlot({
+            ggplot(datasets()) + geom_histogram(aes(x = variables), bins = 30)
+        })
+
+        output$class_plot <- renderPlot({
+            ggplot(datasets()) + geom_bar(aes(x = class), na.rm = TRUE) + coord_flip()
+        })
+
+        output$mode_plot <- renderPlot({
+            ggplot(datasets()) + geom_bar(aes(x = mode), na.rm = TRUE) + coord_flip()
+        })
+
+        output$type_plot <- renderPlot({
+            ggplot(datasets()) + geom_bar(aes(x = type), na.rm = TRUE) + coord_flip()
+        })
     }
 
-    #viewer <- dialogViewer(window_name, width = 1600, height = 1200)
-    #runGadget(ui, server, viewer = viewer)
     shinyApp(ui, server)
 }
